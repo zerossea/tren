525cf831fbd05f0d5d8d2bd8bc11199bca8d2c8d
Zerossea <Zerosea2@yandex.ru>
2024-08-03 23:58:04 +0300

    fix

--- resources/views/program.blade.php
+++ resources/views/program.blade.php
@@ -17,17 +17,12 @@
       @endforeach
     </select>
     <div class="form-group">
-    <label for="training_number">Номер упражнения</label>
+    <label for="training_number">День по счёту</label>
     <select class="form-control w-25 mb-3" id="training_number" name='training_number_id'>
       @foreach($training_numbers as $training_number)
       <option value='{{ $training_number->id }}'>{{ $training_number->training_number }}</option>
       @endforeach
     </select>
-    <label for="weight_max"><h4>Максимальный вес</h4></label>
-    <input value="{{ old('weight_max') }}" type="text" name="weight_max" class="form-control w-25 mb-3" id="weight_max" placeholder="МП">
-    @error("weight_max")
-    <p class="text-danger">{{ $message }}</p>
-    @enderror()
   </div>
   <button type="submit" class="btn btn-primary position-sticky">Добавить</button>
 </form>
